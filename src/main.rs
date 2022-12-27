32118afb413650d9f4fd905a465e753e8641196f
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-27 08:11:56 +0300

    initialize a miniredis client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    //open a connection to a mini-redis address
+    let mut client = client::connect("127.0.0.1:8888").await?;
+
+    //set the key: "hello" with the value: "world"
+    client.set("hello", "world".into()).await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
