9cd6c97f542559d93af393cad2c8000415bf2064
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-27 08:24:01 +0300

    set and get value from server

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use mini_redis::{client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    //open a connection to a mini-redis address
-    let mut client = client::connect("127.0.0.1:8888").await?;
+    //Initialize a min-redis client by openning a connection to a mini-redis address
+    let mut client = client::connect("127.0.0.1:6379").await?;
 
     //set the key: "hello" with the value: "world"
     client.set("hello", "world".into()).await?;
 
-    println!("Hello, world!");
+    //get the key "hello"
+    let result = client.get("hello").await?;
+    
+    println!("Got the value from the server. Result = {:?}", result);
 
     Ok(())
 }
